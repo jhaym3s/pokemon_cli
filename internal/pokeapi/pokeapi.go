064940bde4991d806d6cdabd04744978c096c2ac
Jhaym3s <jhaymescode@gmail.com>
2026-05-24 11:05:54 +0100

    http client

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,5 +1,25 @@
 package pokeapi
 
+import (
+	"net/http"
+	"time"
+)
+
+const baseUrl = "https://pokeapi.co/api/v2/"
+
+type Client struct{
+	httpClient http.Client
+}
+// we do not use the default http client because with that we can not set a timeout
+
+func newClient() Client {
+	return  Client{
+		httpClient: http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
+
 
 type  PokeLocationResponse struct {
 	Count    int         `json:"count"`
